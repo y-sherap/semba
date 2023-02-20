42c4439fd10264caf7bf74583d093e36a7c271d4
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2023-02-19 20:45:25 -0500

    boiler plate

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import Nav from './components/Nav/Nav'
+import Footer from './components/Footer/Footer'
+import HowItWorks from './pages/HowItWorks/HowItWorks'
+import About from './pages/About/About'
+import Home from './pages/Home/Home'
+import FindATherapist from './pages/FindATherapist/FindATherapist'
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Footer />
+      <Routes>
+        <Route path='/' element ={<Home />} />
+        <Route path='/how-it-works' element ={<HowItWorks />} />
+        <Route path='/about' element ={<About />} />
+        <Route path='/fin-a-therapist' element ={<FindATherapist />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
