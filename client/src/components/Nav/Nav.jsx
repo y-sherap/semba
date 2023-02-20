71d5e04508df351517a24483efa1a7282285531f
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2023-02-19 20:56:05 -0500

    nav functionality done

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './nav.css'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <div id='nav'>
+      <div id='left-nav'>
+        <Link to='/'>Semba</Link>
+      </div>
+      <div id='right-nav'>
+        <div><Link to ='/how-it-works'>How It Works</Link></div>
+        <div><Link to ='/about'>About Semba</Link></div>
+        <div><button>Find A Therapist</button></div>        
+      </div>
+    </div>
   )
 }
 
