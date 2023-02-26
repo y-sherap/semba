5ba6a9603aa9e42b65dca5702c200e065e1e4437
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2023-02-26 18:20:08 -0500

    changed banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import BannerImage from '../../assets/Banner.jpg'
 import './banner.css'
+import BannerImage1 from '../../assets/hiker.jpg'
 
 function Banner() {
   return (
     <div id='banner'>
     <div id='banner-text'>
-      <div>Free your mind</div>
-      <div>where ever you are </div> 
+      {/* <div>We care about </div>
+      <div>your mental wellbeing. </div>  */}
+      <div>A brighter future starts</div>
+      <div>with a healthier mind,</div> 
+      <div>online</div> 
+
+
     </div>
     <div>
-    <img src={BannerImage} alt='banner' id='banner-image' />
+    <img src={BannerImage1} alt='banner' id='banner-image' />
     </div>
   </div>
   )
