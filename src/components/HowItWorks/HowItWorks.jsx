97f1bc9314c3ec6959e9c9e3e2c8d37c8d66663a
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2023-02-26 19:18:19 -0500

    updated HowItWorks component

--- src/components/HowItWorks/HowItWorks.jsx
+++ src/components/HowItWorks/HowItWorks.jsx
@@ -1,24 +1,34 @@
 import React from 'react'
 import './howitworks.css'
-import HomeCartoon from '../../assets/HomeCartoon.jpg'
+import Handshake from '../../assets/handshake.png'
+import Communicate from '../../assets/communicate.png'
+
 
 function HowItWorks() {
   return (
-    <div id='how-it-works-container'>
+    <div id='how-it-works-component'>
         <div id='how-it-works-title'>How Semba Works</div>
         <div id='how-it-works-elements'>
-            <div id='how-it-works-left'>
-                <div className='left-item'>Lorem ipsum</div>
-                <div className='left-item'>Lorem ipsum</div>
-                <div className='left-item'>Lorem ipsum</div>
-            </div>
-            <div id='how-it-works-right'>
+            <div id='how-it-works-item'>
+                <div className='left-item'>
+                    <img src={Handshake} alt='cartoon' />
+                </div>
                 <div className='right-item'>
-                    <img src={HomeCartoon} alt='home-cartoon' />
+                    <h2>Get matched to the best therapist for you</h2>
+                    <p>Answer a few questions to find a licensed therapist who fits your needs and preferences. Tap into the largest network of licensed, professional, board-certified providers.</p>
                 </div>
             </div>
+            <div id='how-it-works-item'>
+                <div className='left-item'>
+                    <img src={Communicate} alt='cartoon' />
+                </div>
+                <div className='right-item'>
+                    <h2>Communicate your way</h2>
+                    <p>Talk to your therapist however you feel comfortable — text, chat, phone, or video.</p>
+                </div>
+            </div>
+            </div>
         </div>
-    </div>
   )
 }
 
